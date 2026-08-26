941bd3289500b6912e323a97b68f45c492418a0f
Tatiana7619 <tatianacarvlemes@gmail.com>
2026-08-26 20:33:40 -0300

    Add files via upload
    Exercício 4

---
+++ Exercício 4.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Digite um número:");
+        string num = Console.ReadLine();
+
+        int resultado;
+
+        if(int.TryParse(num, out resultado))
+
+        {
+            Console.WriteLine($"O número digitado: {resultado}");
+        }
+        else
+        {
+            Console.WriteLine("número inválido");
+        }
+    }
+}
