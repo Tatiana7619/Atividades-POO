a75991b15c598f39b83e6d284e978ec642d9feda
Tatiana7619 <tatianacarvlemes@gmail.com>
2026-08-26 20:17:03 -0300

    Add files via upload
    Exercício 2

---
+++ Exercicio 2.cs
@@ -0,0 +1,20 @@
+﻿class Pessoa
+{
+    public string Nome;
+
+}
+class Program
+{
+    static void Main()
+    {
+
+        Pessoa p1 = new Pessoa();
+        p1.Nome = "João";
+
+        Pessoa p2 = p1;
+
+        p2.Nome = "Maria";
+
+        Console.WriteLine(p1.Nome);
+    }
+}
