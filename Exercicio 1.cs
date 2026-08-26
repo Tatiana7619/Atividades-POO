d6ed128c7878303adf3ee8ea485bd4d297a385d7
Tatiana7619 <tatianacarvlemes@gmail.com>
2026-08-26 20:16:08 -0300

    Add files via upload
    Exercício 1

---
+++ Exercicio 1.cs
@@ -0,0 +1,13 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int a = 10;
+        int b = a;
+
+        b = 50;
+
+        Console.WriteLine($"Valor da primeira variável : {a}");
+        Console.WriteLine($"Valor da segunda variável : {b}");
+    }
+}
