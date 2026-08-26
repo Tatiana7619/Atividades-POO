ab0aa091776d04be57a9340d86bf7ddee3318303
Tatiana7619 <tatianacarvlemes@gmail.com>
2026-08-26 20:25:05 -0300

    Add files via upload
    Exercício 3

---
+++ Exercício 3.cs
@@ -0,0 +1,13 @@
+﻿class Program
+{
+    static void Main()
+    {
+        double num = 12.5;
+
+        int valorConvertido = (int)num;
+
+        Console.WriteLine($"Valor antes da conversão: {num}");
+        Console.WriteLine($"Valor após a conversão: {valorConvertido}");
+
+    }
+} 
