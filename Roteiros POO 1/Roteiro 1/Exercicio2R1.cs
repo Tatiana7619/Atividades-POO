b729c08d1fe75053568768e5f4d6b0cc32a92ea4
Tatiana Carvalho <tatianacarvlemes@gmail.com>
2026-09-16 16:07:21 -0300

    Add files via upload
    Exercício 2 do Roteiro 1

---
+++ Roteiros POO 1/Roteiro 1/Exercicio2R1.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class Fantasmas
+{
+    public string Habilidade;
+    public string Nick;
+    public string Cor;
+
+    public void GerarFantasmas()
+    {
+        Console.WriteLine($"Nick: {Nick}, Habilidade: {Habilidade}, Cor: {Cor}");
+
+    }
+    public void Mover(string direcao)
+    {
+        Console.WriteLine($"{Nick} se moveu para {direcao}");
+    }
+
+}
+
+    public class Program
+    {
+        public static void Main()
+        {
+            Fantasmas fantasma1 = new Fantasmas();
+            fantasma1.Nick = "Blinky";
+            fantasma1.Habilidade = "Perseguir Pac-Man";
+            fantasma1.Cor = "Vermelho";
+            fantasma1.GerarFantasmas();
+            fantasma1.Mover("cima");
+
+            Fantasmas fantasma2 = new Fantasmas();
+            fantasma2.Nick = "Pinky";
+            fantasma2.Habilidade = "Prever movimentos de Pac-Man";
+            fantasma2.Cor = "Rosa";
+            fantasma2.GerarFantasmas();
+            fantasma2.Mover("baixo");
+        }
+    }
